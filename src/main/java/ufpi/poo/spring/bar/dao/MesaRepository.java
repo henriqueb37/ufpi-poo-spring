6dc6bdf13aa04b362fae813c2f5d6562db795526
Lucas Assunção <Lucasaspdea@gmail.com>
2025-11-27 14:50:32 -0300

    Mudança na classe MesaRepository para adicionar o jpa repository

--- src/main/java/ufpi/poo/spring/bar/dao/MesaRepository.java
+++ src/main/java/ufpi/poo/spring/bar/dao/MesaRepository.java
@@ -1,7 +1,13 @@
 package ufpi.poo.spring.bar.dao;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import ufpi.poo.spring.bar.model.Mesa;
+import java.util.List;
 
-public interface MesaRepository extends CrudRepository<Mesa, Integer> {
+
+public interface MesaRepository extends JpaRepository<Mesa, Integer> {
+
+    List<Mesa> findByAtivadoTrue();
+
+    List<Mesa> findByEstado(Integer estado);
 }
